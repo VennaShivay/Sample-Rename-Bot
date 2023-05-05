2130ea0d5894b05706d32841f1cba9df2a6a7d73
VennaShiva <125667228+VennaShivay@users.noreply.github.com>
2023-05-05 18:45:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_HASH = environ.get("API_HASH", "09568c14cf0b998cd84d8eedef29dffb")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6080583991:AAGukr_ih6IolgLQAjUW9DC8Tbz-CAN3fz0")
 ADMIN = int(environ.get("ADMIN", "889514276"))
 CAPTION = environ.get("CAPTION", "")
-SUDO_USERS = environ.get("SUDO_USERS", "")
+SUDO_USERS = environ.get("SUDO_USERS", "1493545483 889514276")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
